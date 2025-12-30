ef41ff0fa8cb90c5f0af0615ef213d6ed5ae8a68
indianathe3rd <indiemenziwa@gmail.com>
2025-12-31 00:31:59 +0200

    Refactor twoSum function to correctly find pairs that sum to target

--- twoSum.ts
+++ twoSum.ts
@@ -1,10 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
   let sums: number[] = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] + nums[i + 1] === target) {
-      sums = [i, i + 1];
-    }
+    for (let j = i + 1; j < nums.length; j++)
+      if (nums[i] + nums[j] === target) {
+        sums = [i, j];
+      }
   }
   return sums;
 }
-const theSums = twoSum([2, 7, 15, 15], 30);
+const theSums = twoSum([2, 15, 12, 15], 30);
+console.log(theSums);
